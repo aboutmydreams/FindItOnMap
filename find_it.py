b6dda6b3c350121e2d74b207f6795df64767330c
邓文浩 <aboutmydreams@163.com>
2019-07-29 10:10:09 +0800

    fix a bug and add a sleep time

--- find_it.py
+++ find_it.py
@@ -1,13 +1,15 @@
 import requests
-from app import db, Results
+import time
+# from app import db, Results
+
 
 class BaseSearcher(object):
     query = ''
-    tag = '家电数码'
+    tag = ''
     page_num = 0
     page_size = 20
-    offset = 0.08 # 经纬度的偏移值
-    ak = '0Da09129a81fd709030b0b34995f1e74' # 百度地图key
+    offset = 0.08  # 经纬度的偏移值
+    ak = '0Da09129a81fd709030b0b34995f1e74'  # 百度地图key
     lat = ''
     lng = ''
 
@@ -24,13 +26,13 @@ class BaseSearcher(object):
 
     def create_params(self):
         self.params = {
-                'query': self.query,
-                'tag': self.tag,
-                'page_num': self.page_num,
-                'page_size': self.page_size,
-                'ak': self.ak,
-                'bounds': self.get_bounds(),
-                'output': 'json'}
+            'query': self.query,
+            'tag': self.tag,
+            'page_num': self.page_num,
+            'page_size': self.page_size,
+            'ak': self.ak,
+            'bounds': self.get_bounds(),
+            'output': 'json'}
 
     def get_bounds(self):
         return str(self.lat) + ',' + str(self.lng) + ',' + str(self.lat+self.offset) + ',' + str(self.lng+self.offset)
@@ -58,25 +60,32 @@ class Searcher(BaseSearcher):
 
     # 获取所有范围内的信息
     def get_all_data(self):
-        time = 0
+        times = 0
         while self.lat < self.end_lat:
             while self.lng < self.end_lng:
                 r = self.get_data()
                 data = r.json()
+                print(data)
+                time.sleep(0.5)
+                if data["status"] == 401:
+                    time.sleep(5)
+                    r = self.get_data()
+                    data = r.json()
+                    print(data)
 
                 # 记录上次请求到的数组，避免过多重复请求
                 if data['results'] == self.old_request_data:
                     continue
-                self.old_request_data = data['requests']
+                self.old_request_data = data['results']
 
                 print(data)
-                if data['status']== 1 or data['total'] == 0:
+                if data['status'] == 1 or data['total'] == 0:
                     items = data['results']
                     self.save_data(items)
 
-                    time += 1
+                    times += 1
                     self.page_num = 0
-                    print('get failure    ' + 'time:' + str(time))
+                    print('get failure    ' + 'times:' + str(times))
                     print(self.params)
                 else:
                     self.total_num = data['total']
@@ -86,12 +95,14 @@ class Searcher(BaseSearcher):
                         self.page_num = page_now
                         response = self.get_data()
                         json = response.json()
+                        try:
+                            items = json['results']
+                            self.save_data(items)
 
-                        items = json['results']
-                        self.save_data(items)
-
-                        time+=1
-                        print("time: " + str(time))
+                            times += 1
+                            print("times: " + str(times))
+                        except Exception as e:
+                            print(e)
 
             self.lng = self.lng + self.offset
         self.lat = self.lat + self.offset
@@ -103,10 +114,23 @@ class Searcher(BaseSearcher):
             telephone = ''
             try:
                 telephone = item['telephone']
+                print(name, address, telephone)
             except:
                 pass
-            q = Results.query.filter(Results.address == address).first()
-            if q is None:
-                result = Results(name=name, address=address, telephone=telephone)
-                db.session.add(result)
-                db.session.commit()
+            # print(address)
+
+            # q = Results.query.filter(Results.address == address).first()
+            # if q is None:
+            #     result = Results(name=name, address=address, telephone=telephone)
+            #     db.session.add(result)
+            #     db.session.commit()
+
+
+# searcher = Searcher('手机$数码$通信$通讯', 23.0525730000,
+#                     113.1989640000, 23.2904550000, 113.5010820000)
+# searcher.get_all_data()
+
+
+searcher = Searcher('咖啡', 23.0525730000,
+                    113.1989640000, 23.2904550000, 113.5010820000)
+searcher.get_all_data()
